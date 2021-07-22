0242debc17695bb5f4951d6d0e70559f47aa3e36
alefwhite <alefwhite@hotmail.com>
2021-07-22 16:26:37 -0300

    ok

---
+++ Secao3_Js_LogicaDeProgramacao/Aula32_33AtribuicaoPorDesestruturacao.js
@@ -0,0 +1,24 @@
+// ... rest, ... spread
+//                    0          1          2
+//                 0  1  2    0  1  2    0  1  2
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+
+const [lista1, lista2, lista3] = numeros;
+
+console.log(lista3[2]);
+
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 30,
+    endereco: {
+      rua: 'Av Brasil',
+      numero: 320
+    }
+  };
+  
+// Atribuição via desestruturação
+const { nome, sobrenome, ...resto } = pessoa;
+
+console.log(nome, resto);
