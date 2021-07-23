90d305085b94d9cec1ac36a5cdf6c043190f8bfb
alefwhite <alefwhite@hotmail.com>
2021-07-23 18:01:20 -0300

    ok

--- Secao3_Js_LogicaDeProgramacao/Aula34_ForClassico.js
+++ Secao3_Js_LogicaDeProgramacao/Aula34_ForClassico.js
@@ -1,3 +1,6 @@
+for(let i = 0; i <= 10; i++) 
+    console.log(`${i % 2 === 0 ? 'Par' : 'Impar'}`)
+
 const frutas = ['Maçã', 'Pêra', 'Uva'];
 const novasFrutas = frutas.map(el => el.toLowerCase());
 console.log(novasFrutas); // [ 'maçã', 'pêra', 'uva' ]
